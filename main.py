ac9194c97aebb0cef10d27d7b2541380b09a4e4e
t <jeremyraberdham@gmail.com>
2022-05-03 16:23:38 +0700

    .

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from asyncore import poll3
-from domains import Plane
-import combine
+from pyparsing import nullDebugAction
+from domains import *
+from combine import *
+import string
 
 ###
 # Planes
@@ -8,6 +9,12 @@ import combine
 p1 = Plane('Boeing 757', 'VN457')
 p2 = Plane('Boeing 347', 'VN522')
 p3, p4, p5, p6, p7, p8
+
+###
+# Flights
+###
+f1 = Flight('VN457', 0, 0, p1, 16, 20, 'Hanoi, Vietnam', 'Phu Quoc, Vietnam')
+f2, f3, f4, f5
 
 def main():
     print('Main')

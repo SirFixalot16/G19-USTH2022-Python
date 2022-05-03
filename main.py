6174857dffd4f9cd13e353631792768ff0e183cc
t <jeremyraberdham@gmail.com>
2022-05-03 16:17:16 +0700

    Added main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-import domains
+from asyncore import poll3
+from domains import Plane
 import combine
+
+###
+# Planes
+###
+p1 = Plane('Boeing 757', 'VN457')
+p2 = Plane('Boeing 347', 'VN522')
+p3, p4, p5, p6, p7, p8
 
 def main():
     print('Main')
+    
 
 if __name__ == "__main__":
     main()
